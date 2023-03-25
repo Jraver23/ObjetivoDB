1d95243bac0b5acd46b07bee9dc219b1cc9bea78
Jorge <jcarrenoo@uoc.edu>
2023-03-25 18:34:43 +0100

    Se crea la clase ClientePremium

--- src/model/Cliente_estandar.java
+++ src/model/Cliente_estandar.java
@@ -1,5 +1,6 @@
 package model;
 
+// subclase que hereda las propiedades y metodos de Cliente
 public class Cliente_estandar extends Cliente {
     public Cliente_estandar (String nombre, String domicilio, String nif, String email){
         super(nombre, domicilio, nif, email);
