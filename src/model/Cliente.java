3d3fdcdcb6af02b94444f8577f900d92d6a737f3
Jorge <jcarrenoo@uoc.edu>
2023-03-25 17:32:54 +0100

    Creacion de clase cliente

---
+++ src/model/Cliente.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Cliente {
+    private String nombre;
+    private String domicilio;
+    private String nif;
+    private String email;
+
+    public Cliente(String nombre, String domicilio, String nif, String email) {
+        this.nombre = nombre;
+        this.domicilio = domicilio;
+        this.nif = nif;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
