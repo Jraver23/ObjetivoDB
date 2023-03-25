bffed0e8153e1a4ca8af9edf7a83dd973ecc75a9
Jorge <jcarrenoo@uoc.edu>
2023-03-25 18:24:21 +0100

    Create Cliente_estandar.java

---
+++ src/model/Cliente_estandar.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Cliente_estandar extends Cliente {
+    public Cliente_estandar (String nombre, String domicilio, String nif, String email){
+        super(nombre, domicilio, nif, email);
+    }
+
+    // Aqui van los constructores
+}
