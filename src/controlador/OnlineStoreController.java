ab241d8d73942b3ae19acacc52a913273fe078c7
nferron14 <nferron@uoc.edu>
2023-03-26 18:25:40 +0200

    iniciando con el controlador

---
+++ src/controlador/OnlineStoreController.java
@@ -0,0 +1,10 @@
+package controlador;
+import java.util.List;
+import ObjetivoDB.model.Student;
+
+public class OnlineStoreController {
+        private Datos datos;
+        public Controlador() {
+            datos = new Datos ();
+        }
+}
