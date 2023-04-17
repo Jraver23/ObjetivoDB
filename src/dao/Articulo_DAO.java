c6c90081f73b3e1589afdd454abe021dcbf2d623
serx3 <serx31994@hotmail.com>
2023-04-17 09:06:28 +0200

    Update ArticuloDAO y ClienteDAO completo

--- src/dao/Articulo_DAO.java
+++ src/dao/Articulo_DAO.java
@@ -1,4 +1,139 @@
 package dao;
 
+import model.Articulo;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Articulo_DAO {
+    connectionDAO Manager = new connectionDAO();
+    private String select = "SELECT * FROM articulo WHERE codigo=?;";
+    private String insert = "INSERT INTO articulo (codigo, descripcion, precio_venta, gestion_envio, tiempo_preparacion) VALUES (?, ?, ?, ?, ?);";
+    private String update = "UPDATE articulo SET descripcion=?, precio_venta=?, gestion_envio=?, tiempo_preparacion=? WHERE codigo=?;";
+    private String delete = "DELETE FROM articulo WHERE codigo=?;";
+    private String selectall = "SELECT * FROM articulo;";
+
+    private Connection connection;
+
+    public Articulo_DAO(Connection connection) {
+        try {
+            this.connection = Manager.getConnection();
+        } catch (SQLDataException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public PreparedStatement getStatement(String query) {
+        try {
+            return this.connection.prepareStatement(query);
+        }
+        catch (SQLException e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+
+    public Articulo select(String s) {
+        try {
+            PreparedStatement ps = getStatement(select);
+            ps.setString(1, s);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                String codigo = rs.getString("codigo");
+                String descripcion = rs.getString("descripcion");
+                Float precio_venta = rs.getFloat("precio_venta");
+                Float gastos_envio = rs.getFloat("gestion_envio");
+                Long tiempo_preparacion = rs.getLong("tiempo_preparacion");
+                return new Articulo(codigo, descripcion, precio_venta, gastos_envio, tiempo_preparacion);
+
+            }
+            ps.close();
+            return null;
+
+        }
+        catch (SQLException e) {
+            System.out.println("Error al seleccionar " + e);
+            return null;
+        }
+    }
+
+
+
+    public void insert(Articulo articulo) {
+        try {
+            PreparedStatement ps = getStatement(insert);
+            ps.setString(1, articulo.getCodigoalfanumerico());
+            ps.setString(2, articulo.getDescripcion());
+            ps.setFloat(3, articulo.getPrecio_de_venta());
+            ps.setFloat(4, articulo.getGastos_de_envio());
+            ps.setLong(5, articulo.getTiempo_de_preparacion());
+            ps.executeUpdate();
+            ps.close();
+
+        }
+        catch (SQLException e) {
+            System.out.println("Error al insertar " + e);
+        }
+    }
+
+
+    public void update(Articulo articulo) {
+        try {
+            PreparedStatement ps = getStatement(update);
+            ps.setString(1, articulo.getDescripcion());
+            ps.setFloat(2, articulo.getPrecio_de_venta());
+            ps.setFloat(3, articulo.getGastos_de_envio());
+            ps.setLong(4, articulo.getTiempo_de_preparacion());
+            ps.setString(5, articulo.getCodigoalfanumerico());
+            ps.executeUpdate();
+            ps.close();
+
+        }
+        catch (SQLException e) {
+            System.out.println("Error al ejecutar el update " + e);
+        }
+    }
+
+    @Override
+    public void delete(Articulo articulo) {
+        try {
+            PreparedStatement ps = getStatement(delete);
+            ps.setString(1, articulo.getCodigoalfanumerico());
+            ps.executeUpdate();
+            ps.close();
+
+        }
+        catch (SQLException e) {
+            System.out.println("Error al eliminar " + e);
+        }
+    }
+
+
+
+    public ArrayList<Articulo> selectall() {
+        ArrayList<Articulo> articulos = new ArrayList();
+        try {
+            PreparedStatement ps = getStatement(selectall);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                String codigo = rs.getString("codigo");
+                String descripcion = rs.getString("descripcion");
+                Float precio_venta = rs.getFloat("precio_venta");
+                Float gastos_envio = rs.getFloat("gestion_envio");
+                Long tiempo_preparacion = rs.getLong("tiempo_preparacion");
+                articulos.add(new Articulo(codigo, descripcion, precio_venta, gastos_envio, tiempo_preparacion));
+            }
+            ps.close();
+            return articulos;
+        }
+        catch (SQLException e) {
+            System.out.println("Error al seleccionar todos " + e);
+            return null;
+        }
+    }
+
+
 }
---
+++ src/dao/Cliente_DAO.java
@@ -0,0 +1,162 @@
+package dao;
+
+import model.Articulo;
+import model.Cliente;
+import model.ClienteEstandar;
+import model.ClientePremium;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class Cliente_DAO {
+
+    connectionDAO Manager = new connectionDAO();
+    private String select = "SELECT * FROM cliente WHERE email=?;";
+    private String insert = "INSERT INTO cliente (nombre, domicilio, nif, email, tipo_cliente) VALUES (?, ?, ?, ?, ?);";
+    private String update = "UPDATE cliente SET nombre=?, domicilio=?, nif=?, email=? WHERE tipo_cliente=?;";
+    private String delete = "DELETE FROM cliente WHERE email=?;";
+    private String selectall = "SELECT * FROM cliente;";
+
+    private Connection connection;
+
+    public Cliente_DAO(Connection connection) {
+        try {
+            this.connection = Manager.getConnection();
+        } catch (SQLDataException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public PreparedStatement getStatement(String query) {
+        try {
+            return this.connection.prepareStatement(query);
+        }
+        catch (SQLException e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+
+    public Cliente select(String s) {
+        try {
+            PreparedStatement ps = getStatement(select);
+            ps.setString(1, s);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                String nombre = rs.getString("nombre");
+                String domicilio = rs.getString("domicilio");
+                String nif = rs.getString("nif");
+                String email = rs.getString("email");
+                String tipoCliente = rs.getString("tipo_cliente");
+
+                if (tipoCliente.equals("Estandar")){
+                    return new ClienteEstandar(nombre, domicilio, nif, email);
+                }
+                else if (tipoCliente.equals("Premium")){
+                    return new ClientePremium( nombre, domicilio, nif, email);
+                }
+                else {
+                    return null;
+                }
+            }
+            ps.close();
+            return null;
+        }
+        catch (SQLException e) {
+            System.out.println("Error al seleccionar " + e);
+            return null;
+        }
+    }
+
+
+
+    public void insert(Cliente cliente) {
+        try {
+            PreparedStatement ps = getStatement(insert);
+            ps.setString(1, cliente.getNombre());
+            ps.setString(2, cliente.getDomicilio());
+            ps.setString(3, cliente.getNif());
+            ps.setString(4, cliente.getEmail());
+            ps.setString(5, cliente.tipoCliente());
+            ps.executeUpdate();
+            ps.close();
+
+        }
+        catch (SQLException e) {
+            System.out.println("Error al insertar " + e);
+        }
+    }
+
+
+    public void update(Cliente cliente) {
+        try {
+            PreparedStatement ps = getStatement(update);
+            ps.setString(1, cliente.getNombre());
+            ps.setString(2, cliente.getDomicilio());
+            ps.setString(3, cliente.getNif());
+            ps.setString(4, cliente.getEmail());
+            ps.setString(5, cliente.tipoCliente());
+            ps.executeUpdate();
+            ps.close();
+
+        }
+        catch (SQLException e) {
+            System.out.println("Error al ejecutar el update " + e);
+        }
+    }
+
+
+    public void delete(Cliente cliente) {
+        try {
+            PreparedStatement ps = getStatement(delete);
+            ps.setString(1, cliente.getEmail());
+            ps.executeUpdate();
+            ps.close();
+
+        }
+        catch (SQLException e) {
+            System.out.println("Error al eliminar " + e);
+        }
+    }
+
+
+
+    public ArrayList<Cliente> selectall() {
+        ArrayList<Cliente> clientes = new ArrayList();
+        try {
+            PreparedStatement ps = getStatement(selectall);
+            ResultSet rs = ps.executeQuery();
+            Cliente cliente = null;
+            while(rs.next()) {
+                String nombre = rs.getString("nombre");
+                String domicilio = rs.getString("domicilio");
+                String nif = rs.getString("nif");
+                String email = rs.getString("email");
+                String tipoCliente = rs.getString("tipo_cliente");
+
+                if (tipoCliente.equals("Estandar")){
+                    cliente = new ClienteEstandar(nombre, domicilio, nif,email);
+                }
+                else if (tipoCliente.equals("Premium")){
+                    cliente = new ClientePremium(nombre, domicilio, nif,email);
+                }
+                else {
+                    return null;
+                }
+            }
+            clientes.add(cliente);
+            ps.close();
+            return null;
+        }
+        catch (SQLException e) {
+            System.out.println("Error al seleccionar " + e);
+            return null;
+        }
+    }
+
+
+}
+
