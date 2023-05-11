02bde11cb12d0f8ed221449a4174a7a949464178
Jorge <jcarrenoo@uoc.edu>
2023-05-11 12:55:43 +0200

    cambios en main

---
+++ src/model/HibernateUtil.java
@@ -0,0 +1,29 @@
+package model;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory =
+            buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        SessionFactory sessionFactory = null;
+        try {
+            //Create the configuration object.
+            Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+            System.out.println("Contraseña de la BBDD: " + configuration.getProperties().getProperty("hibernate.connection.password"));
+            // Get the SessionFactory object from configuration.
+            sessionFactory = configuration.buildSessionFactory();
+        }
+        catch (Exception e) {
+            System.out.println("Error: "+e);
+        }
+        return sessionFactory;
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
